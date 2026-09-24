68e897d4e149f2ff205a9cfadb95a86737067967
Emmanuel Garcia <z8vjmcsttp@privaterelay.appleid.com>
2026-09-24 17:22:55 -0500

    FInished Numpy Arrays
    Added functions to calculate days with temperatures above 90 degrees and updated main function to use these new calculations.

--- temperature_anaylsis.py
+++ temperature_anaylsis.py
@@ -1,6 +1,7 @@
 import json
 import os
 from pathlib import Path
+import numpy as np
 
 def load_weather_data():
     with open(os.path.join(Path(__file__).parent.resolve(), 'ep_weather_data.json'), 'r') as file:
@@ -55,9 +56,18 @@ def monthly_max(month):
 
     return maximum
 
+def above_90(array):
+    return np.sum(array > 90)
+
+
+def days_above_90(month):
+    daily_max = np.max(month, axis=1)
+    return above_90(daily_max)
+    
 def main():
     yearly_data = load_weather_data()
-
+    yearly_array = np.array(yearly_data)
+    
     september_15 = yearly_data[8][14]
     print("September 15th Min =", min(september_15),
           "Max =", max(september_15),
@@ -80,5 +90,41 @@ def main():
 
     print("August Min =", monthly_min(yearly_data[7]),
           "Max =", monthly_max(yearly_data[7]))
+    print()
+
+    september_15_array = yearly_array[8][14]
+    print("September 15th Min =", np.min(september_15_array),
+          "Max =", np.max(september_15_array),
+          "Mean =", np.mean(september_15_array))
+
+    january_1_array = yearly_array[0][0]
+    print("January 1st Min =", np.min(january_1_array),
+          "Max =", np.max(january_1_array),
+          "Mean =", np.mean(january_1_array))
+
+    december_30_array = yearly_array[11][29]
+    print("December 30th Min =", np.min(december_30_array),
+          "Max =", np.max(december_30_array),
+          "Mean =", np.mean(december_30_array))
+    print()
+
+    print("February Min =", np.min(yearly_array[1]),
+          "Max =", np.max(yearly_array[1]),
+          "Mean =", np.mean(yearly_array[1]))
+
+    print("August Min =", np.min(yearly_array[7]),
+          "Max =", np.max(yearly_array[7]),
+          "Mean =", np.mean(yearly_array[7]))
+
+    print()
+
+    print("There were", days_above_90(yearly_array[4]),
+          "days above 90 in May")
+
+    print("There were", days_above_90(yearly_array[7]),
+          "days above 90 in August")
+
+    print("There were", days_above_90(yearly_array[10]),
+          "days above 90 in November")
 
 main()
